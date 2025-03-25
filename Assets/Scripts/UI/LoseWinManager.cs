f392d3f3485ea3e48ba3b09b5ad1dcd26c23688c
Luiz-RX <22295673@live.uem.es>
2025-03-26 00:54:38 +0100

    LVL_2 y cambios de escena

---
+++ Assets/Scripts/UI/LoseWinManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseWinManager : MonoBehaviour
+{
+    [SerializeField] private String levelName;
+    [SerializeField] private String currentLevelName;
+
+    public void GoMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoNextLevel()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+    public void ReloadLevel() 
+    {
+        SceneManager.LoadScene(currentLevelName);
+    }
+}
